f5bfa1c0f3739890e28d96edde70811850df02e5
DavidingPlus <2065666169@qq.com>
2023-10-12 09:40:24 +0800

    update File_Transfer 10.12

---
+++ test/client.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file client.cpp
+ * @brief 客户端的主程序
+ * @author lzx0626 (2065666169@qq.com)
+ * @version 1.0
+ * @date 2023-10-12
+ *
+ * @copyright Copyright (c) 2023  电子科技大学
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
